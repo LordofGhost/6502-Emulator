627749c0a1f55ed3a866b1aadcaf557b8dabfa52
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-05-28 15:32:37 +0200

    refactor: rename Tools class to Convert

--- src/tools/Convert.cpp
+++ src/tools/Convert.cpp
@@ -1,6 +1,6 @@
-#include "Tools.h"
+#include "Convert.h"
 
-char Tools::binToHex(Byte i) {
+char Convert::binToHex(Byte i) {
     if (i >= 0 && i <= 9) return i + 48;
     if (i >= 10 && i <= 15) return i + 55;
     return '?';
