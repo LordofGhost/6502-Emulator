d7cfa67a232ce325fc4d9b429b46fb8aa989919f
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-05-28 15:34:03 +0200

    feat: create emulator clock, which stores and handles the clock cycles

---
+++ src/components/CrystalOscillator.h
@@ -0,0 +1,40 @@
+#ifndef CRYSTALOSCILLATOR_H
+#define CRYSTALOSCILLATOR_H
+
+
+
+class CrystalOscillator {
+private:
+  unsigned long cycles = 0;
+public:
+  CrystalOscillator& operator++() {
+    cycles++;
+    return *this;
+  }
+
+  unsigned long getCycles() const {
+    return cycles;
+  }
+
+  void increment() {
+    cycles++;
+  }
+
+  void increment(const unsigned long &cycleCount) {
+    cycles += cycleCount;
+  }
+
+  void reset() {
+    cycles = 0;
+  }
+
+  std::string toString() const {
+    return "-- Clock --\n"
+           "Cycle count: " + std::to_string(cycles) + '\n' +
+           "-----------";
+  }
+};
+
+
+
+#endif //CRYSTALOSCILLATOR_H
